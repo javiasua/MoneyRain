b61b3452725c3667f32b6a45d9c732b23c1eb72f
Javier Asua <asuajavier@gmail.com>
2020-05-17 10:24:24 +0200

    Third commit

--- game.js
+++ game.js
@@ -3,16 +3,28 @@ var ctx = canvas.getContext('2d')
 ctx.fillStyle = 'border:2px solid black';
 var img = new Image();
 var money = new Image();
-img.src ='emptyMoney.png';
-money.src = 'moneybag.png';
+img.src ='basket.png';
+money.src = 'gold-coins.png';
 var bombImage = new Image();
 bombImage.src = 'bomb1.png'
 explosionImage = new Image();
 explosionImage.src = 'explosion.png'
 var liveimage = new Image();
 liveimage.src= 'live.png'
-let speedBool=false;
 let score = 0;
+let lives = 3;
+let increasebombSpeed = 1;
+let moneyBagSpeed = 2;
+var goldScore = new Image();
+goldScore.src = 'gold.png'
+let background = new Image();
+background.src = 'background.png'
+let taxes = new Image()
+taxes.src = 'taxes.png'
+
+let checker = 10;
+let emptyBagSpeed = 50
+
 
 var emptyBag = {
     x : 10,
@@ -21,47 +33,87 @@ var emptyBag = {
     height : 100,
 
     moveLeft : function(){
-        this.x -= 50;
+        this.x -= emptyBagSpeed;
     } ,
 
     moveRight : function(){
-        this.x +=50;
+        this.x +=emptyBagSpeed;
     }
 }
 
-var bombsArr = [{
-    x : Math.floor(Math.random()*500),
-    y :  10,
-    width : 50,
-    height : 50,
-    moveDown : function(){
-        this.y += 2;
-    },
-    checkCollision : function(){
-        if(this.y+60>canvas.height-emptyBag.height && this.x>emptyBag.x && this.x<emptyBag.x+emptyBag.width){
-            let hi = this.y;
-            ctx.drawImage(explosionImage,this.x,hi,60,60)
-            this.y += NaN;
+
+class fallingObject{
+    constructor(x,height,width,type){
+        this.x = x;
+        this.y =10;
+        this.height=height;
+        this.width=width;
+        this.type = type;
+        this.bool = true;
+        this.explosionBool = false;
+        this.explosionX = x;
+
+    }
+    moveDown(){
+        switch(this.type){
+            case 'bomb':
+                this.y +=moneyBagSpeed+increasebombSpeed;
+                break;
+            case 'money':
+                this.y += moneyBagSpeed;
+                break;
+            case 'live':
+                this.y += moneyBagSpeed;
+                break;
         }
     }
-}]
 
-let moneyBagSpeed =2;
 
-var moneyBagArr = [{
-    x: Math.floor(Math.random()*500),
-    y : 10,
-    width : 70,
-    height  : 70,
-    moveDown : function(){
-        this.y += 2;
-    },
-    checkCollision : function(){
-        if(this.y+60>canvas.height-emptyBag.height && this.x>emptyBag.x && this.x<emptyBag.x+emptyBag.width){
+    checkCollision(){
+        if(this.y+30>canvas.height-emptyBag.height && this.y+90<canvas.height+30 && this.x+this.width/2>emptyBag.x && this.x<emptyBag.x+emptyBag.width){
             this.y += NaN;
+            if(this.type == 'money'){
+                score++;
+            }
+            if(this.type=='bomb'){
+                this.explosionBool = true;
+                switch(lives){
+                    case 3:
+                        ctx.clearRect(370,15,50,40)
+                        lives--
+                        break;
+                    case 2:
+                        ctx.clearRect(430,15,50,40)
+                        lives--;
+                        break;
+                    case 1:
+                        ctx.clearRect(490,15,50,40)
+                        lives--
+                        break
+                    case 0:
+                        document.location.href = "gameOver.html";
+                        break;
+                }
+            }
+            if(this.type=='live'){
+                if(lives<=2){
+                    lives++
+                }
+            }
         }
     }
-}];
+
+}
+
+function updateSpeeds(){
+    if(score>checker){
+        checker+=10;
+        moneyBagSpeed +=1;
+        emptyBagSpeed +=5;
+    }
+}
+var fallingObject1 = new fallingObject(Math.floor(Math.random()*500),70,70,'money')
+var fallingObjectArr = [fallingObject1]
 
 
 document.onkeydown = function(e) {
@@ -80,59 +132,88 @@ document.onkeydown = function(e) {
   
 }
 
-function drawEmptyMoneyBag(emptyBag){
-    ctx.drawImage(img, emptyBag.x, emptyBag.y, emptyBag.width, emptyBag.height)  
-    console.log('helloMan')      
-}
-function drawMoneyBag(moneyBag){
-    ctx.drawImage(money, moneyBag.x, moneyBag.y, moneyBag.width, moneyBag.height)        
+function drawBackground() {
+    ctx.drawImage(background,0,0,canvas.width,canvas.height)
 }
 
-function drawBombs(bombi){
-    ctx.drawImage(bombImage,bombi.x,bombi.y,bombi.width,bombi.height);
+function drawEmptyMoneyBag(emptyBag){
+    ctx.drawImage(img, emptyBag.x, emptyBag.y, emptyBag.width, emptyBag.height)      
+}
+
+function drawFallinObjects(fallingObject1){
+    if(fallingObject1.type=='money'){
+        ctx.drawImage(money, fallingObject1.x, fallingObject1.y, fallingObject1.width, fallingObject1.height)        
+    }  
+    if(fallingObject1.type=='bomb'){
+        ctx.drawImage(bombImage, fallingObject1.x, fallingObject1.y, fallingObject1.width, fallingObject1.height)        
+    }   
+    if(fallingObject1.type=='live'){
+        ctx.drawImage(liveimage, fallingObject1.x, fallingObject1.y, fallingObject1.width, fallingObject1.height)        
+    }    
+    if(fallingObject1.type=='taxes'){
+        ctx.drawImage(taxes, fallingObject1.x, fallingObject1.y, fallingObject1.width, fallingObject1.height)        
+    }    
 }
 
 function drawLives(){
-    for(let i=0;i<3;i++){
-        ctx.drawImage(liveimage,370+i*60,15,50,40);
+    switch(lives){
+        case 3:
+            ctx.drawImage(liveimage,370,15,50,40);
+            ctx.drawImage(liveimage,430,15,50,40);
+            ctx.drawImage(liveimage,490,15,50,40);
+            break;
+        case 2: 
+            ctx.drawImage(liveimage,430,15,50,40);
+            ctx.drawImage(liveimage,490,15,50,40);
+            break;
+        case 1:
+            ctx.drawImage(liveimage,490,15,50,40);
+            break;
     }
 }
 
 function drawScoreBoard(){
     ctx.font = '30px Arial'
-    ctx.fillText('Score : '+score,20,40)
+    ctx.fillText('Score : '+score*50,20,40)
+    ctx.drawImage(goldScore,200,10,40,40)
+}
+
+function drawExplosion(x,y,width,height,bombBool){
+        ctx.drawImage(explosionImage,x,y,width,height)
 }
 
 function draw (){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    console.log(emptyBag);
+    drawBackground()
     drawLives()
     drawEmptyMoneyBag(emptyBag);
-    console.log('hello')
     drawScoreBoard();
-    drawBags();
-    for(let i =0; i<bombsArr.length;i++){
-        drawBombs(bombsArr[i]);
-        
-        bombsArr[i].moveDown();
-        bombsArr[i].checkCollision();
-        if(bombsArr[i].y == 190 ){
-            bombsArr.push({
-                x: Math.floor(Math.random()*500),
-                y: 10,
-                width: 50,
-                height :50,
-                moveDown : function(){
-                    this.y += moneyBagSpeed;
-                },
-                checkCollision : function(){
-                    if(this.y+60>canvas.height-emptyBag.height && this.x>emptyBag.x && this.x<emptyBag.x+emptyBag.width){
-                        ctx.drawImage(explosionImage,this.x,this.y+25,60,60)
-                        this.y += NaN;
-                        score++;
-                    }
+    updateSpeeds()
+    for(let i =0; i<fallingObjectArr.length;i++){
+        drawFallinObjects(fallingObjectArr[i]);
+        let explosionY = canvas.height-emptyBag.height;
+        //drawExplosion(fallingObjectArr[i].x,explosionY,fallingObjectArr[i].width,fallingObjectArr[i].height,fallingObjectArr[i].explosionBool)
+        fallingObjectArr[i].moveDown();
+        fallingObjectArr[i].checkCollision();
+        if(fallingObjectArr[i].y > 150  && fallingObjectArr[i].bool){
+            fallingObjectArr[i].bool = false;
+            if(Math.floor(Math.random()*8) > 2){
+                fallingObject1 = new fallingObject(Math.floor(Math.random()*500),70,70,'money')
+                fallingObjectArr.push(
+                fallingObject1
+            )}else{
+                if(lives>=3){
+                    fallingObject1 = new fallingObject(Math.floor(Math.random()*500),70,70,'bomb')
+                    fallingObjectArr.push(
+                    fallingObject1
+                    )
+                } else{
+                    fallingObject1 = new fallingObject(Math.floor(Math.random()*500),70,70,(Math.round(Math.random()*5)<4?'bomb':'live'))
+                    fallingObjectArr.push(
+                    fallingObject1
+                    )
                 }
-            })
+            }
         }
 
     }
@@ -140,34 +221,6 @@ function draw (){
 
 }
 
-function drawBags(){
-    for(let i = 0; i<moneyBagArr.length;i++){
-        drawMoneyBag(moneyBagArr[i]);
-        moneyBagArr[i].moveDown();
-        moneyBagArr[i].checkCollision();
-        //console.log(i)
-        console.log(moneyBagArr[i].y)
-        if(moneyBagArr[i].y ===180){
-            console.log('hello')
-             moneyBagArr.push({
-                 x: Math.floor(Math.random()*500),
-                 y : 10,
-                 width : 60,
-                 height  : 60,
-                 moveDown : function(){
-                     this.y += moneyBagSpeed;
-                 },
-                 checkCollision : function(){
-                     if(this.y+60>canvas.height-emptyBag.height && this.x>emptyBag.x && this.x<emptyBag.x+emptyBag.width){
-                         this.y += NaN;
-                         score++;
-                     }
-                 }
-             })
-         }
- 
-     }
-}
 
 let interval = setInterval(function(){
     draw();
@@ -178,3 +231,4 @@ let interval = setInterval(function(){
         //console.log('hello')
         speedBool = true;
     }*/
+
